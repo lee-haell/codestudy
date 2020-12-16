98a959bc36c78c70465f583b2240e91b4180eac1
lee-haell <haeeell@naver.com>
2020-12-16 14:58:42 +0900

    21.반복문 while, 조건문

---
+++ main.js
@@ -0,0 +1,14 @@
+//반복문 while, 조건문
+document.write('<p>1</p>');
+
+let i = 0;
+
+while(i < 3) { 
+    //i값이 처음 0이면 true, 아래 코드 실행 후 +1되어 1 >> 총 3번 반복됨
+    document.write('<p>2</p>');
+    document.write('<p>3</p>');
+    i = i + 1;
+}
+
+document.write('<p>4</p>');
+//2까지 반복출력되고 다음 코드 실행
