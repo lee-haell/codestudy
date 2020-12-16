ea1a2bb7ca3769e3604ebc3c84f272daee672e7f
lee-haell <haeeell@naver.com>
2020-12-16 15:22:51 +0900

    반복문에 배열 활용

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//반복문 while, 조건문
+/* 반복문 while, 조건문
 document.write('<p>1</p>');
 
 let i = 0;
@@ -12,3 +12,15 @@ while(i < 3) {
 
 document.write('<p>4</p>');
 //2까지 반복출력되고 다음 코드 실행
+*/
+
+/* 반복문에 배열 활용 */
+let color = [ "red", "orange", "yellow", "green" ];
+
+let i = 0;
+
+while(i < color.length){ //배열에 데이터가 추가 또는 삭제되어도 변수의 길이만큼 반복되기 때문에 상관x
+    document.write('<li>' + color[i] + '</li>');
+    //color[i] ->> 변수의 배열 출력
+    i = i + 1;
+}
